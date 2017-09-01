093493ee33add54bbbb12fec01496a5b11a53839
Glenn 'devalias' Grant <glenn@devalias.net>
2017-09-01 13:23:34 +1000

    Update libgobuster apex PoC to return relevant results via proper mechanism

--- poc-apex/functions/libgobuster/main.go
+++ poc-apex/functions/libgobuster/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/json"
 
-	// "fmt"
+	"fmt"
 	// "os"
 
 	"github.com/OJ/gobuster/libgobuster"
@@ -22,6 +22,7 @@ func ConfigureGobuster(url string) (*libgobuster.State, *multierror.Error) {
 	s.Mode = "dir"
 	s.Wordlist = "words.txt"
 	s.Url = url
+	s.Quiet = true
 
 	extensions = ""
 	codes = "200,204,301,302,307"
@@ -34,17 +35,60 @@ func ConfigureGobuster(url string) (*libgobuster.State, *multierror.Error) {
 	}
 }
 
+// type ResultStruct struct {
+// 	url    string
+// 	status int
+// }
+
 func main() {
 	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
-		var out string
+		var results []string
+		// var results []ResultStruct
 
+		// Defining this here so we can close around the 'results' slice, inside the handler function
+		var captureDirResultForApex = func(s *libgobuster.State, r *libgobuster.Result) {
+			output := ""
+
+			if s.StatusCodes.Contains(r.Status) || s.Verbose {
+				if s.Expanded {
+					output += s.Url
+				} else {
+					output += "/"
+				}
+				output += r.Entity
+
+				if !s.NoStatus {
+					output += fmt.Sprintf(" (Status: %d)", r.Status)
+				}
+
+				if r.Size != nil {
+					output += fmt.Sprintf(" [Size: %d]", *r.Size)
+				}
+				output += "\n"
+
+				// r := ResultStruct{
+				// 	url:    r.Entity,
+				// 	status: r.Status,
+				// }
+
+				// fmt.Fprintln(os.Stderr, output)
+				results = append(results, output)
+				// results = append(results, r)
+			}
+		}
+
+		// Configure libgobuster
 		state, err := ConfigureGobuster("http://devalias.net/")
 		if err.ErrorOrNil() != nil {
 			return nil, err
 		}
 
+		// State is setup/considered valid. Now we can override things..
+		state.Printer = captureDirResultForApex
+
+		// Run libgobuster
 		libgobuster.Process(state)
 
-		return out, nil
+		return results, nil
 	})
 }
