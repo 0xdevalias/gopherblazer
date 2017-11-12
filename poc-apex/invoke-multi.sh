ab7d3fb80d22e2e2a042d527ccda26d5a2c4439c
Glenn 'devalias' Grant <glenn@devalias.net>
2017-11-12 17:15:01 +1100

    Update invoke-multi to slice automagically

--- poc-apex/invoke-multi.sh
+++ poc-apex/invoke-multi.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+DEFAULT_SLICES=2
 DEFAULT_URL="http://test-discovery.gopherblazer.devalias.net"
 DEFAULT_DICT="words.txt"
 DEFAULT_THREADS=10
 
-URL=${1:-$DEFAULT_URL}
-DICT=${2:-$DEFAULT_DICT}
-THREADS=${3:-$DEFAULT_THREADS}
+SLICE_CNT=${1:-$DEFAULT_SLICES}
+URL=${2:-$DEFAULT_URL}
+DICT=${3:-$DEFAULT_DICT}
+THREADS=${4:-$DEFAULT_THREADS}
 
 function jsonTemplate {
   echo "{\"url\": \"$URL\", \"wordlist\":\"$DICT\", \"threads\": $THREADS, \"sliceStart\": $1, \"sliceEnd\": $2}"
@@ -17,51 +19,36 @@ function invokeApexSlice {
 }
 
 function invokeMulti {
-  # Example for words.txt
-  invokeApexSlice 1 5
-  invokeApexSlice 6 10
-  invokeApexSlice 11 17
+  # Count words
+  DICT_SIZE=$(wc -l ./functions/libgobuster/$DICT | awk '{print $1}')
+  SLICE_SIZE=$(($DICT_SIZE / $SLICE_CNT))
+  SLICE_EXCESS=$(($DICT_SIZE % $SLICE_CNT))
 
-  # Example for big.txt
-  # invokeApexSlice 1     2000
-  # invokeApexSlice 2001  4000
-  # invokeApexSlice 4001  6000
-  # invokeApexSlice 6001  8000
-  # invokeApexSlice 8001  10000
-  # invokeApexSlice 10001 12000
-  # invokeApexSlice 12001 14000
-  # invokeApexSlice 14001 16000
-  # invokeApexSlice 16001 18000
-  # invokeApexSlice 18001 20000
-  # invokeApexSlice 20001 20469
+  echo "$DICT contains $DICT_SIZE lines."
+  echo "  $SLICE_CNT slices will be $SLICE_SIZE lines each (evenly) with $SLICE_EXCESS leftover"
+  echo -e "  (Remember 0 index counts as a line..)\n"
 
-  # Example 2 for big.txt
-  # invokeApexSlice 1     1000
-  # invokeApexSlice 1001  2000
-  # invokeApexSlice 2001  3000
-  # invokeApexSlice 3001  4000
-  # invokeApexSlice 4001  5000
-  # invokeApexSlice 5001  6000
-  # invokeApexSlice 6001  7000
-  # invokeApexSlice 7001  8000
-  # invokeApexSlice 8001  9000
-  # invokeApexSlice 9001  10000
-  # invokeApexSlice 10001 11000
-  # invokeApexSlice 11001 12000
-  # invokeApexSlice 12001 13000
-  # invokeApexSlice 13001 14000
-  # invokeApexSlice 14001 15000
-  # invokeApexSlice 15001 16000
-  # invokeApexSlice 16001 17000
-  # invokeApexSlice 17001 18000
-  # invokeApexSlice 18001 19000
-  # invokeApexSlice 19001 20000
-  # invokeApexSlice 20001 20469
+  TOTAL_SLICES=0
+  while [ $TOTAL_SLICES -lt $SLICE_CNT ]; do
+    if [ $TOTAL_SLICES -eq 0 ]; then
+      SLICE_FROM=0
+      SLICE_TO=$(($SLICE_FROM+$SLICE_SIZE+$SLICE_EXCESS-1))
+    else
+      SLICE_FROM=$((SLICE_TO+1))
+      SLICE_TO=$(($SLICE_FROM+$SLICE_SIZE-1))
+    fi
+
+    if [ $SLICE_FROM -lt $DICT_SIZE ]; then
+      echo "Slice #$((TOTAL_SLICES+1)): $SLICE_FROM to $SLICE_TO ($(($SLICE_TO-$SLICE_FROM+1)) lines) "
+      invokeApexSlice $SLICE_FROM $SLICE_TO
+    else
+      echo "Slice #$((TOTAL_SLICES+1)) : Skipping slice, outside dictionary range" #$SLICE_FROM to $SLICE_TO ($(($SLICE_TO-$SLICE_FROM+1)) lines)"
+    fi
+
+    let TOTAL_SLICES=$(($TOTAL_SLICES+1))
+  done
 
   wait
 }
-
-# Count words
-echo "$DICT contains $(wc -l ./functions/libgobuster/$DICT | awk '{print $1}') lines."
 
 time invokeMulti && echo "This timing is for the whole job"
